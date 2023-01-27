e1de14cb6dbde39ba9bdd602152736058328a7e6
Blair Fleming <blair3003.bf@gmail.com>
2023-01-27 11:18:52 +0000

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import Fixtures from "./class/Fixtures.js"
+
+console.log("%cFooty Fixtures ⚽", "color:blue; font-size:16px")
+console.log("//////////////////")
+
+// Get the fixture list
+const fixtures = Fixtures.instance.fixtures
+
+// Display the fixture list
+
+// ???
+
+console.log(fixtures)
