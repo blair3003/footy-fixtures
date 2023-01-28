5cf5de9b42c446b5d162a51214ad193e8afa9522
Blair Fleming <blair3003.bf@gmail.com>
2023-01-28 14:37:05 +0000

    second phase

--- js/funcs/api.js
+++ js/funcs/api.js
@@ -17,3 +17,24 @@ export const fetchFixtures = async () => {
 		console.error(err)
 	}
 }
+
+export const fetchFixturesWithIds = async (ids) => {
+	console.log('Fetching data with ids')
+
+	const idsString = ids.slice(0, 20).join('-')
+
+	const url = `https://v3.football.api-sports.io/fixtures?ids=${idsString}`
+	const options = {
+		headers: {
+			'x-rapidapi-host': 'v3.football.api-sports.io',
+			'x-rapidapi-key': API_FOOTBALL_KEY
+		}
+	}
+	try {
+		const fixturesStream = await fetch(url, options)
+		const fixturesJson = await fixturesStream.json()
+		return fixturesJson.response
+	} catch (err) {
+		console.error(err)
+	}
+}
--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ import {
 	getNewData,
 	isDataStale
 } from './funcs/data.js'
+import {
+	fetchFixturesWithIds
+} from './funcs/api.js'
 
 console.log("%cFooty Fixtures ⚽", "color:blue; font-size:16px")
 console.log("//////////////////")
@@ -11,55 +14,99 @@ console.log("//////////////////")
 console.log(`API Key: ${API_FOOTBALL_KEY}`)
 console.log("//////////////////")
 
+const app = () => {
 
-// Get the fixture list
-// 1. Get local data from storage
-let data = getLocalData()
+	// Get the fixture list
+	// 1. Get local data from storage
+	let data = getLocalData()
 
-// 2. Check if local data is stale
-const dataIsStale = isDataStale(data)
+	// 2. Check if local data is stale
+	const stale = isDataStale(data)
 
-// 3. If no local data or data is stale, get new data
-if (!data || dataIsStale) {
-	console.log('Data is missing or stale!')
-	data = getNewData()
+	// 3. If no local data or data is stale, get new data
+	if (!data || stale) {
+		console.log('Data is missing or stale!')
+		data = getNewData()
+	}
+
+	console.log(data)
+
+
+
+	// Check todays fixtures
+
+
+
+	// Filter fixtures to only those before todays date and time, and status set to NS
+
+	const notStarted = data.fixtures.filter(
+		obj =>
+			obj.fixture.status.short === 'NS'
+		// &&	obj.fixture.timestamp * 1000 < Date.now()
+	)
+
+	console.log('notStarted')
+	console.log(notStarted)
+
+
+	// If any not started before current time:
+	//// Get todays fixtures
+
+	////// Fixture ids
+
+	const notStartedIds = notStarted.map(obj => obj.fixture.id)
+
+	////// Call to api for fixtures
+
+	// const updatedNotStartedFixtures = fetchFixturesWithIds(notStartedIds)
+	const updatedNotStartedFixtures = notStarted
+
+	// console.log('updatedNotStartedFixtures')
+	// console.log(updatedNotStartedFixtures)
+
+
+	//// Update fixture list
+
+	const fixturesWithoutNotStarted = data.fixtures.filter(obj => !notStartedIds.includes(obj.fixture.id))
+
+	console.log('fixturesWithoutNotStarted')
+	console.log(fixturesWithoutNotStarted)
+
+
+	const fixtures = [ ...fixturesWithoutNotStarted, ...updatedNotStartedFixtures ]
+	data = { ts: Date.now(), fixtures }
+
+	console.log('data')
+	console.log(data)
+
+	//// Save fixtures to local storage
+	// setLocalData(data)
+
+
+
+
+	// TODO: REFACTOR ABOVE
+
+
+
+
+
+
+	// If any in play before current time:
+
+	//// Get in play fixtures
+
+	//// Update fixture list
+
+	//// Save fixtures to local storage
+
+
+
+
+	// Display the fixture list
+
+	// ???
 }
 
-console.log(data)
+document.addEventListener("DOMContentLoaded", app);
 
-
-
-
-
-// Get full season fixtures
-
-// Save fixtures to local storage
-
-
-
-// Check todays fixtures
-
-
-// If any not started before current time:
-
-//// Get todays fixtures
-
-//// Update fixture list
-
-//// Save fixtures to local storage
-
-
-// If any in play before current time:
-
-//// Get in play fixtures
-
-//// Update fixture list
-
-//// Save fixtures to local storage
-
-
-
-
-// Display the fixture list
-
-// ???
